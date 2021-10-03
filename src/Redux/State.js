03865ce2cfc9bfbceadd0c0b648a09488f065e92
hanterdam123 <afilatov880@gmail.com>
2021-10-03 11:01:58 +0300

    first change

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -3,7 +3,7 @@ let State = {
     MessagePage: 
     {
       DialogsInfo: [
-        {name:'Nikit', id:1, photo:'https://st3.depositphotos.com/1007566/32981/v/950/depositphotos_329811158-stock-illustration-young-man-avatar-character-icon.jpg' },
+        {name:'Nikita', id:1, photo:'https://st3.depositphotos.com/1007566/32981/v/950/depositphotos_329811158-stock-illustration-young-man-avatar-character-icon.jpg' },
         {name:'Arthur', id:2, photo: 'https://implantes-dentales.odonto.com.ar/wp-content/uploads/2018/01/Depositphotos_32817757_l.jpg' },
         {name:'Masha', id:3, photo:'https://yobte.ru/uploads/posts/2019-11/krasivye-devushki-s-dlinnymi-volosami-60-foto-43.jpg' }
       ],
